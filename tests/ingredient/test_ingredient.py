dbfb3002590c6f2468dae5f8f5845b69a8eb2e30
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-18 10:29:01 -0300

    Projeto Restaurant-Orders - Requisito 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("massa de lasanha")
+    ingredient3 = Ingredient("queijo mussarela")
+
+    assert ingredient1.name == "queijo mussarela"
+
+    assert ingredient1.name == ingredient3.name
+    assert ingredient1.name != ingredient2.name
+
+    assert ingredient1 != ingredient2
+    assert ingredient1 == ingredient3
+
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
+    assert hash(ingredient1) == hash(ingredient3)
+    assert hash(ingredient1) != hash(ingredient2)
+    assert ingredient2.restrictions == {
+        Restriction.LACTOSE, Restriction.GLUTEN
+        }

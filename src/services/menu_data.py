5f06ca8364985ab4b86a78dc6e39ff419f421787
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-20 14:20:44 -0300

    Projeto Restaurant-Orders - Requisito 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -5,12 +5,12 @@ from models.dish import Dish, Ingredient
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.source_path = source_path
-        self.dishes = self.load_menu_data(source_path)
+        self.dishes = set()
+        self.ingredients = set()
+        self.load_menu_data()
 
-    def load_menu_data(self, source_path: str) -> set:
-        dishes = []
-
-        with open(source_path, encoding='utf-8') as csvfile:
+    def load_menu_data(self):
+        with open(self.source_path, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 dish_name = row['dish']
@@ -18,14 +18,25 @@ class MenuData:
                 ingredient_name = row['ingredient']
                 recipe_amount = int(row['recipe_amount'])
 
-                dish = Dish(dish_name, price)
+                dish = self._get_or_create_dish(dish_name, price)
+                ingredient = self._get_or_create_ingredient(ingredient_name)
 
-                if dish not in dishes:
-                    dishes.append(dish)
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
 
-                dish.add_ingredient_dependency(
-                    Ingredient(ingredient_name),
-                    recipe_amount,
-                )
+    def _get_or_create_dish(self, name: str, price: float) -> Dish:
+        dish = next(
+            (plate for plate in self.dishes if plate.name == name),
+            None)
+        if dish is None:
+            dish = Dish(name, price)
+            self.dishes.add(dish)
+        return dish
 
-        return dishes
+    def _get_or_create_ingredient(self, name: str) -> Ingredient:
+        ingredient = next(
+            (ing for ing in self.ingredients if ing.name == name),
+            None)
+        if ingredient is None:
+            ingredient = Ingredient(name)
+            self.ingredients.add(ingredient)
+        return ingredient
